2b4c48b72d5c2c706e191bb2491e786c56f01914
Shubham Singh <77970687+shubham2338@users.noreply.github.com>
2023-04-14 13:02:02 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Remove the balls/remove-the-balls.py
@@ -0,0 +1,53 @@
+from typing import List
+
+
+class Solution:
+    def finLength(self, n : int, color : List[int], radius : List[int]) -> int:
+        # code her
+        st=[]
+        i=0
+        while i<n:
+            if len(st) and st[-1][0]==color[i] and st[-1][1]==radius[i]:
+                st.pop()
+            else:
+                st.append([color[i],radius[i]])
+            i+=1
+        
+        return len(st)
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        color=IntArray().Input(N)
+        
+        
+        radius=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.finLength(N, color, radius)
+        
+        print(res)
+        
+
+# } Driver Code Ends
